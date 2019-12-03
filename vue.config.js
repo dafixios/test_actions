b53ad88f6e3699e28408fbfbbc3ac7b4e3a5d0d3
Hector <hector@ignisoftware.com>
2019-12-03 18:47:45 +0000

    Webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-var path = require('path')
-const webpack = require('webpack')
-const webpackConfig = require('./webpack.config')
+var path = require("path")
+const webpack = require("webpack")
+const webpackConfig = require("./webpack.development.config")
 
 module.exports = {
-  configureWebpack: webpackConfig,
-  publicPath: '/sites/all/modules/custom/m6_platform/vue_apps/dist'
+  configureWebpack: process.env.NODE_ENV === "production" ? {} : webpackConfig,
+  publicPath: "/sites/all/modules/custom/m6_platform/vue_apps/dist"
 }
