238de355bbfe375a15171d5394fe0e07ac8e2084
Hector Fierro <hector@ignisoftware.com>
2019-12-03 14:35:14 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,4 @@ const webpackConfig = require("./webpack.development.config")
 
 module.exports = {
   configureWebpack: webpackConfig,
-  publicPath: "/sites/all/modules/custom/m6_platform/vue_apps/dist"
 }
